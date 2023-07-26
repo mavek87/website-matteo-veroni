bc97434c9886d619a110ad85e6ad0abd8ff76c7e
veroni <matteo.veroni@giuffre.it>
2023-07-26 21:39:40 +0200

    cleaning of webpack file config

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = {
                                                             attributes: [
                                                                 { xmlns: "http://www.w3.org/2000/svg" }
                                                             ]
-                                                        },
+                                                        }
                                                     }
                                                 }
                                             }
